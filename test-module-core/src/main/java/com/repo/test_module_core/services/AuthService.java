3f93640416d4c01eb938fe1ced85e8f4612f7fbc
dinesh-cherrylabs <dineshkumar.s@asista.com>
2021-05-21 18:10:36 +0530

    Initial commit

---
+++ test-module-core/src/main/java/com/repo/test_module_core/services/AuthService.java
@@ -0,0 +1,4 @@
+package com.repo.test_module_core.services;
+
+public class AuthService {
+}
---
+++ test-module-ui/src/main/java/com/repo/test_module_ui/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.repo.test_module_ui.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.repo.test_module_ui.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
